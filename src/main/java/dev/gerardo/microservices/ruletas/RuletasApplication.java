1393bb08e6abaa9edf6fc941aa7c2705faaa3bb7
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-21 09:49:28 -0600

    modify: Adding support for eureka server

--- src/main/java/dev/gerardo/microservices/ruletas/RuletasApplication.java
+++ src/main/java/dev/gerardo/microservices/ruletas/RuletasApplication.java
@@ -2,8 +2,10 @@ package dev.gerardo.microservices.ruletas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RuletasApplication {
 
 	public static void main(String[] args) {
